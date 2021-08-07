13aac45a0fccfb5c73d88501bcfcad367aa4c585
Rahul Mahato <65120414+rm02021999@users.noreply.github.com>
2021-08-07 18:53:45 +0530

    Add files via upload

--- clock.py
+++ clock.py
@@ -9,6 +9,6 @@ sched = BlockingScheduler()
 # Schedule job_function to be called every two hours
 #sched.add_job(automate_message, 'date', run_date='2021-07-31 21:40:00')
 #sched.add_job(automate_message, 'date', run_date=datetime(2021, 8, 2, 17, 10 , 5))
-sched.add_job(automate_message,'cron',day_of_week='mon-fri',hour=6, minute=50 )
+sched.add_job(automate_message,'cron',day_of_week='mon-sun',hour=19, minute=10 )
 
 sched.start()
