42360e597dd3ce8ede214dc548d9b82311da7596
Rahul Mahato <65120414+rm02021999@users.noreply.github.com>
2021-08-08 22:37:20 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -6,6 +6,6 @@ sched = BlockingScheduler()
 
 #sched.add_job(automate_message,'interval',hour=2)      # if you want to send send message in every 2 hours 
 #sched.add_job(automate_message, 'date', run_date='2021-07-31 21:40:00')     # if you want to send send message at specific date and time
-sched.add_job(automate_message,'cron',day_of_week='mon-sun',hour=11, minute=10 )
+sched.add_job(automate_message,'cron',day_of_week='mon-sun',hour=23, minute=40 )
 
 sched.start()
